3c5fa5a8343e868dbe57edfcf94d7fbef8f543f9
Carlos Castellanos Vera <ccverak@gmail.com>
2019-04-05 20:11:16 +0200

    Adding the second version

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'The first version'
+      message: 'The second version'
     })
   };
   return callback(null, response);
